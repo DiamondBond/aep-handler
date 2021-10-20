940673b5290d73cd4f7dc335d49b05e06fc1851f
Diamond Bond <diamondbond1@gmail.com>
2021-10-20 19:55:53 +0530

    hotfix
    Signed-off-by: Diamond Bond <diamondbond1@gmail.com>

--- jsx/aftereffects.jsx
+++ jsx/aftereffects.jsx
@@ -1,27 +1,27 @@
-﻿#include "json2.js"
+﻿//@#include "json2.js"
 
 function getPreviewPaths(path) {
-    var thisFolder = Folder(path);
-    var files = thisFolder.getFiles();
+  var thisFolder = Folder(path);
+  var files = thisFolder.getFiles();
 
-    var fileNames = [];
+  var fileNames = [];
 
-    for(var i = 0; i < files.length; i++){
-        if(files[i].name.indexOf(".png") != -1){
-            fileNames.push(files[i].fsName);
-        }
+  for (var i = 0; i < files.length; i++) {
+    if (files[i].name.indexOf(".png") != -1) {
+      fileNames.push(files[i].fsName);
     }
+  }
 
-    return JSON.stringify(fileNames);
-    }
+  return JSON.stringify(fileNames);
+}
 
 function osCheck() {
-        var os = $.os;
-        var match = os.indexOf("Windows");
-        if(match != (-1)) {
-                var userOS = "PC";
-            } else {
-                 var userOS = "MAC";
-                }
-            return userOS;
-    }
+  var os = $.os;
+  var match = os.indexOf("Windows");
+  if (match != -1) {
+    var userOS = "PC";
+  } else {
+    var userOS = "MAC";
+  }
+  return userOS;
+}
